ab72f879c164b290b42e89a3f9df0fd80a3b0ba4
--list <dian.ivanov@lights.digital>
2023-10-04 19:48:26 +0300

    Fix amount & add waiting for some blockchain trasnactions

--- evm-bridge-cli/src/database/entities/tokenburned.entity.ts
+++ evm-bridge-cli/src/database/entities/tokenburned.entity.ts
@@ -3,7 +3,7 @@ import { AbstractEventEntity } from './abstractevent.entity';
 
 @Entity()
 export class TokenBurned  extends AbstractEventEntity {
-  constructor(amountOwner: string, sourceTokenAddress: string, burnedTokenAddress: string, amount: number, blockchainName: string) {
+  constructor(amountOwner: string, sourceTokenAddress: string, burnedTokenAddress: string, amount: bigint, blockchainName: string) {
     super(amountOwner, amount, blockchainName);
     this.sourceTokenAddress = sourceTokenAddress;
     this.burnedTokenAddress = burnedTokenAddress;
--- evm-bridge-cli/src/database/entities/tokenclaimed.entity.ts
+++ evm-bridge-cli/src/database/entities/tokenclaimed.entity.ts
@@ -3,7 +3,7 @@ import { AbstractEventEntity } from './abstractevent.entity';
 
 @Entity()
 export class TokenClaimed  extends AbstractEventEntity {
-    constructor(amountOwner: string, sourceTokenAddress: string, claimedTokenAddress: string, amount: number, blockchainName: string) {
+    constructor(amountOwner: string, sourceTokenAddress: string, claimedTokenAddress: string, amount: bigint, blockchainName: string) {
         super(amountOwner, amount, blockchainName);
         this.sourceTokenAddress = sourceTokenAddress;
         this.claimedTokenAddress = claimedTokenAddress;
